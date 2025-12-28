8d8b885efdad2d3da074c75a1cc35199aee1408b
leo9iota <leo9iota@gmail.com>
2025-12-28 22:30:56 +0100

    feat(cli): rename add command to init

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,19 +3,17 @@ import { Command } from 'commander';
 
 import { init } from './commands/init';
 
-const program = new Command();
+const cli = new Command();
 
-program
-    .name('markdown-editor-cli')
+cli.name('markdown-editor-cli')
     .description('CLI to install the Modular Markdown Editor')
     .version('0.1.0');
 
-program
-    .command('init')
+cli.command('init')
     .description('Initialize the markdown editor component in your project')
     .option('-p, --path <path>', 'Installation path (default: components/markdown-editor)')
     .option('-o, --overwrite', 'Overwrite existing files', false)
     .option('--no-deps', 'Skip dependency installation')
     .action(init);
 
-program.parse();
+cli.parse();
